cbdc004f3c38345b976f4fd9c02b34987444be29
nevolinaa <arina@nevolin.info>
2023-12-21 13:09:28 +0300

    Add infer,train files and data

--- infer.py
+++ infer.py
@@ -1,23 +1,45 @@
-import pandas as pd 
 import joblib
-from sklearn.metrics import mean_squared_error
+import mlflow
+import numpy as np
+import pandas as pd
+from mlflow import MlflowClient
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
-def make_prediction(test_dataset='X_test.csv', model_path='model.joblib'):
+def make_prediction(test_dataset="X_test.csv", model_path="model.joblib"):
     X_test = pd.read_csv(test_dataset)
+    X_test = X_test.to_numpy()
 
     with open(model_path, "rb") as file:
-         model = joblib.load(file)
+        model = joblib.load(file)
     prediction = model.predict(X_test)
+    params = model.get_params()
 
-    return prediction
+    return prediction, params
 
-def get_mse_score(test_target='target.csv'):
-    y_test = pd.read_csv(test_dataset)
-    y_pred = make_prediction()
+
+def get_scores(test_target="target.csv"):
+    y_test = pd.read_csv(test_target)
+    y_pred, params = make_prediction()
+
+    y_pred_df = pd.DataFrame(y_pred)
+    y_pred_df.to_csv("predictions.csv", index=False)
+
     mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
 
-    return f'test MSE: {mse}'
+    metrics = {"mae": mae, "mse": mse, "rmse": rmse, "r2": r2}
+
+    with mlflow.start_run(run_name="House prices") as run:
+        mlflow.log_params(params)
+        mlflow.log_metrics(metrics)
+
+    return metrics
+
 
 if __name__ == "__main__":
-    get_mse_score()
+    client = MlflowClient(tracking_uri="http://127.0.0.1:8080")
+    client.create_experiment(name="House prices")
+    get_scores()
--- train.py
+++ train.py
@@ -1,58 +1,79 @@
-import pandas as pd 
+import os
+
+import hydra
 import joblib
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
+import pandas as pd
+from omegaconf import DictConfig
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 
-def open_data(path="data/housing_price_dataset.csv"):
+def open_data(path):
     df = pd.read_csv(path)
 
     return df
 
-def prepare_data(encoder, scaler, df, test_size, random_state, encode_columns, y_column):
+
+def prepare_data(
+    encoder, scaler, df, test_size, random_state, encode_columns, y_column
+):
     df[encode_columns] = encoder.fit_transform(df[encode_columns])
-   
-    X = df.drop(columns = [y_column], axis = 1)
+
+    X = df.drop(columns=[y_column], axis=1)
     y = df[y_column]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                        test_size = test_size, 
-                                                        random_state = random_state)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    return X, y, X_train, X_test
+    return X, y, X_train, X_test, y_train, y_test
 
-def train_model(model, X_train):
+
+def train_model(model, X_train, y_train):
     model.fit(X_train, y_train)
 
     return model
 
+
 def save_model(path, model):
     with open(path, "wb") as file:
         joblib.dump(model, file, compress=3)
-    
+
     return model
 
-def prepare_data_and_model():
-    df = open_data()
+
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def prepare_data_and_model(cfg: DictConfig):
+    df = open_data(cfg.data.path)
 
     encoder = LabelEncoder()
     scaler = StandardScaler()
-    X, y, X_train, X_test = prepare_data(encoder, scaler, df, 0.3, 42, 'Neighborhood', 'Price')
-    X_test.to_csv ('X_test.csv', index= False)
-    y.to_csv ('target.csv', index= False)
+    X, y, X_train, X_test, y_train, y_test = prepare_data(
+        encoder,
+        scaler,
+        df,
+        cfg.model.test_size,
+        cfg.model.random_state,
+        cfg.model.encode_columns,
+        cfg.model.target_column,
+    )
+
+    X_test_df = pd.DataFrame(X_test)
+    X_test_df.to_csv(cfg.save_names.x, index=False)
+    y_df = pd.DataFrame(y_test)
+    y_df.to_csv(cfg.save_names.y, index=False)
 
     model = LinearRegression()
-    model = train_model(model, X_train)
-    model = save_model('model.joblib', model)
+    model = train_model(model, X_train, y_train)
+    model = save_model(cfg.model.save_path, model)
 
 
 if __name__ == "__main__":
+    os.system("dvc remote add --default myremote \
+                           gdrive://1XFxIlCiJriQTO2Xp2C-3jSAlDCNN5dFY")
+    os.system("dvc pull --remote myremote")
     prepare_data_and_model()
-
